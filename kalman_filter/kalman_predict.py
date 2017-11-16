3de84f7c1377487a3f594edde511b0f1cd314e50
HAlmawi <hasan.almawi@gmail.com>
2017-11-15 22:05:50 -0500

    Kalman filter update and predict done.

--- kalman_filter/kalman_predict.py
+++ kalman_filter/kalman_predict.py
@@ -2,23 +2,33 @@ import sys
 import math
 sys.path.append('../tools/')
 import math_functions
+
 # Kalman filter needs to predict the quaternions to get rotation matrix
 
 # Step 1.  Estimate the current state based on all previous states and the gyro measurement
 
-def predict_state(g,prev_state,delta_t,rate_k):
+def predict_state(F,prev_state):
     #Compute state transition model F
+    pred_state = math_functions.matrix_multiply(F,prev_state)
+    return pred_state
+
+# Step 2. Estimate the a priori error covariance matrix based on the previous error covariance matrix
+def predict_priori_covariance(prev_cov,Q_variance,F):
+    pred_cov = math_function.matrix_addition(math_function.matrix_multiply(math_function.matrix_multiply(F,prev_cov),math_functions.transpose_matrix(F)),Q_variance)
+    return pred_cov
+
+def get_F_matrix(g,delta_t):
     F1 = [[1],[-1*delta_t/2*g[0][0]],[-1*delta_t/2*g[1][0]],[-1*delta_t/2*g[2][0]]]
     F2 = [[delta_t/2*g[0][0]],[1],[delta_t/2*g[2][0]],[-1*delta_t/2*g[1][0]]]
     F3 = [[delta_t/2*g[1][0]],[-1*delta_t/2*g[2][0]],[1],[delta_t/2*g[1][0]]]
     F4 = [[-1*delta_t/2*g[2][0]],[delta_t/2*g[1][0]],[-1*delta_t/2*g[0][0]],[1]]
     F = [F1,F2,F3,F4]
+    return F
 
-    pred_state = math_functions.matrix_multiply(F,prev_state)
-    return pred_state, F
-
-# Step 2. Estimate the a priori error covariance matrix based on the previous error covariance matrix
-def predict_priori_covariance(prev_cov,imu_variance,F):
-    Q = {{imu_variance, 0},{0,bias_variance}}*delta_t
-    pred_cov = math_function.matrix_addition(math_function.matrix_multiply(math_function.matrix_multiply(F,prev_cov),math_functions.transpose_matrix(F)),Q)
-    return pred_cov
+def get_Q_matrix(g_var):
+    Q1 = [[g_var[0][0]+g_var[1][0]+g_var[2][0]],[-1*g_var[0][0]+g_var[1][0]-g_var[2][0]],[-1*g_var[0][0]-g_var[1][0]+g_var[2][0]],[g_var[0][0]-g_var[1][0]-g_var[2][0]]]
+    Q2 = [[-1*g_var[0][0]+g_var[1][0]-g_var[2][0]],[g_var[0][0]+g_var[1][0]+g_var[2][0]],[g_var[0][0]-g_var[1][0]-g_var[2][0]],[-1*g_var[0][0]-g_var[1][0]+g_var[2][0]]]
+    Q3 = [[-1*g_var[0][0]-g_var[1][0]+g_var[2][0]],[g_var[0][0]-g_var[1][0]-g_var[2][0]],[g_var[0][0]+g_var[1][0]+g_var[2][0]],[-1*g_var[0][0]+g_var[1][0]-g_var[2][0]]]
+    Q4 = [[g_var[0][0]-g_var[1][0]-g_var[2][0]],[-1*g_var[0][0]+g_var[1][0]-g_var[2][0]],[-1*g_var[0][0]+g_var[1][0]-g_var[2][0]],[g_var[0][0]+g_var[1][0]+g_var[2][0]]]
+    Q = [Q1,Q2,Q3,Q4]
+    return Q
--- kalman_filter/kalman_update.py
+++ kalman_filter/kalman_update.py
@@ -1,3 +1,8 @@
+import sys
+import math
+sys.path.append('../tools/')
+import math_functions
+
 # Kalman filter needs to update based on errors from prediction
 
 # Step 1. Compute the difference between the measurement and the a priori state to get the innovation
@@ -5,22 +10,31 @@ def get_innovation(measurement,pred_state):
     return measurement - pred_state
 
 # Step 2. Calculate the innovation covariance
-def calc_innovation_covariance(prev_cov,measurement_cov):
-    H = {1,0}
-    S = H*prev_cov*H + measurement_cov
+def calc_innovation_covariance(prev_cov,sigma_R):
+    H = math_functions.get_identity_matrix(4)
+    R = get_measurement_cov(sigma_R)
+    S = math_functions.matrix_addition(math_functions.matrix_multiplication(math_functions.matrix_multiplication(H,prev_cov),math_functions.transpose_matrix(H)),R)
     return S
 
 # Step 3. Calculate the Kalman gain to determine how much we trust the innovation
 def calc_Kalman_gain(prev_cov, innovation_cov):
-    H = {1,0}
-    return prev_cov*H*(1/innovation_cov)
+    H = math_functions.get_identity_matrix(4)
+    return math_functions.matrix_multiplication(prev_cov,math_functions.matrix_multiplication(math_functions.transpose_matrix(H),math_functions.invert_4x4_matrix(innovation_cov)))
 
 # Step 4. Update the a posteriori estimate of the current state
 def update_posteriori(pred_state,kalman_gain,innovation):
-    return pred_state + kalman_gain*innovation
+    return math_functions.matrix_addition(pred_state,math_functions.matrix_multiplication(kalman_gain,innovation))
 
 # Step 5. Update the a posteriori error covariance matrix
 def update_posteriori_covariance(kalman_gain,prev_cov):
-    I = {{1,0},{0,1}}
-    H = {1,0}
-    return (I-kalman_gain*H)*prev_cov
+    I = math_functions.get_identity_matrix(4)
+    H = math_functions.get_identity_matrix(4)
+    return math_functions.matrix_multiplication(math_functions.matrix_subtraction(I,math_functions.matrix_multiplication(kalman_gain,H)),prev_cov)
+
+def get_measurement_cov(sigma_R):
+    matrix = [[0 for x in range(4)] for y in range(4)]
+    matrix[0][0] = sigma_R[0][0]
+    matrix[1][1] = sigma_R[1][0]
+    matrix[2][2] = sigma_R[2][0]
+    matrix[3][3] = sigma_R[3][0]
+    return matrix
